8fcb103532e432337adc7f29861ebbc0b8588331
StanDarius24 <standarius24@gmail.com>
2020-10-02 17:38:02 +0300

    Pagina principala

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaginaComponent } from './paginaprincipala/pagina/pagina.component';
+import { HeaderComponent } from './paginaprincipala/componente/header/header.component';
+import { FooterComponent } from './paginaprincipala/componente/footer/footer.component';
+import { NavigationComponent } from './paginaprincipala/componente/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
